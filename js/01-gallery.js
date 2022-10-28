57378a4d561dd8dbcaea788372ad5264821f5539
Toma1402 <toma.k1402@gmail.com>
2022-10-28 13:49:51 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+console.log(galleryRef);
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+galleryRef.insertAdjacentHTML("afterbegin", markup);
+
+galleryRef.addEventListener("click", inModal);
+function inModal(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance =
+    basicLightbox.create(`<img width="1400" height="900" src="${evt.target.dataset.source}">
+	`);
+
+  instance.show();
+  galleryRef.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
